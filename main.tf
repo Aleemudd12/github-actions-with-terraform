a72eb910f3d9c54a55153c8eebc19c8d2347ada4
aleemudd12 <aleemnu@gmail.com>
2024-03-26 22:30:57 +0530

    commit to change

--- main.tf
+++ main.tf
@@ -1,21 +1,8 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
+  
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = ami-080e1f13689e07408
   instance_type = var.instance_type
 
   tags = {
